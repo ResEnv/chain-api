a8e79fa671157c897487ba3b113b47bf2c7fe33d
Spencer Russell <spencer.f.russell@gmail.com>
2013-09-12 04:29:30 +0400

    adds metric and unit to sensor data resource

--- doppel2/core/api.py
+++ doppel2/core/api.py
@@ -6,3 +6,9 @@ class ScalarDataResource(ModelResource):
     class Meta:
         queryset = ScalarData.objects.all()
         resource_name = 'scalar_data'
+        excludes = ['id']
+
+    def dehydrate(self, bundle):
+        bundle.data['unit'] = bundle.obj.unit.name
+        bundle.data['metric'] = bundle.obj.metric.name
+        return bundle
--- doppel2/core/tests.py
+++ doppel2/core/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from doppel2.core.models import ScalarData, Unit, Metric
 from datetime import datetime
 from django.db.models import Avg
+import json
 
 BASE_API_URL = '/api/v1/'
 
@@ -46,10 +47,30 @@ class SensorDataTest(TestCase):
 
 
 class ApiTest(TestCase):
+    def setUp(self):
+        unit = Unit(name='C')
+        unit.save()
+        metric = Metric(name='Temperature')
+        metric.save()
+        data = ScalarData(unit=unit, metric=metric, value=25)
+        data.save()
+
     def test_base_uri_should_be_gettable(self):
         base_response = self.client.get(BASE_API_URL,
                                         Accept="application/json")
         self.assertEqual(base_response.status_code, 200)
+
+    def test_scalar_data_should_be_gettable_from_api(self):
+        base_response = self.client.get(BASE_API_URL + "scalar_data/",
+                                        Accept="application/json")
+        self.assertEqual(base_response.status_code, 200)
+        data = json.loads(base_response.content)['objects']
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]['value'], 25)
+        self.assertEqual(data[0]['unit'], 'C')
+        self.assertEqual(data[0]['metric'], 'Temperature')
+
+
 
 
 
