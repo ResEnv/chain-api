89e1ec8b81d38b349b5bd2174dcd2e269cb72903
Spencer Russell <sfr@mit.edu>
2014-02-09 04:52:47 +0400

    adds serialization cacheing for a 40% speedup on huge data collection GETs

--- doppel2/settings.py
+++ doppel2/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'doppel2.core',
     'south',
+    'django_extensions',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
