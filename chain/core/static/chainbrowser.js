837ed00c534aba0081c742210b529cfcfc7ce5b9
Spencer Russell <spencer.f.russell@gmail.com>
2014-03-25 23:11:59 +0400

    updates HTML render to use new hal+json API

---
+++ chain/core/static/chainbrowser.js
@@ -0,0 +1,147 @@
+/*
+{% macro show_list(val) -%}
+    <ul class="list-group">
+        {% for subval in val %}
+            <li class="list-group-item">{{ show_value(subval) }}</li>
+        {% endfor %}
+    </ul> <!-- list-group -->
+{%- endmacro %}
+
+{% macro show_value(val) -%}
+    {% if val.items  %}
+        {{ show_dict(val, gen_id()) }}
+    {% elif val.__iter__ %}
+        {{ show_list(val) }}
+    {% else %}
+        {{ val }}
+    {% endif %}
+{%- endmacro %}
+
+{% macro show_dict(val, unique, collapsed=True) -%}
+    {% if '_href' in val and not '_type' in val %}
+        <!-- just a link -->
+        <a href={{ val._href }}>{{ val._disp }}</a>
+    {% else %}
+        <!-- either an embedded resource or normal dict -->
+        {% if '_disp' in val %}
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a class="list-group-item-header" href={{ val._href }}>{{ val._disp }}</a>
+                    <button type="button" class="btn btn-xs pull-right" data-toggle="collapse" data-target="#{{ unique }}">
+                        <span class="caret"></span>
+                    </button>
+                </div> <!-- panel-heading -->
+            {% if collapsed %}
+            <ul class="list-group collapse" id="{{ unique }}">
+            {% else %}
+            <ul class="list-group collapse in" id="{{ unique }}">
+            {% endif %}
+        {% else %}
+            <ul class="list-group">
+        {% endif %}
+            {% for key, subval in val.items() %}
+                {% if key[0] != '_' %}
+                    <li class="list-group-item">
+                        {{ key }}: {{ show_value(subval) }}
+                    </li>
+                {% endif %}
+            {% endfor %}
+        </ul>
+        {% if '_disp' in val %}
+            </div> <!-- panel -->
+        {% endif %}
+    {% endif %}
+{%- endmacro %}
+*/
+
+function render_chart(raw_data, element) {
+    $("#chart-header").removeClass("hidden")
+
+    data = []
+    for(i = 0; i < raw_data.length; i++) {
+        new_point = {
+            x: Date.parse(raw_data[i].timestamp) / 1000,
+            y: raw_data[i].value
+        }
+        data.push(new_point)
+    };
+
+    //datachart = $("<div/>").id("data-chart");
+    //element.append(datachart)
+    var graph = new Rickshaw.Graph( {
+        element: document.querySelector("#data-chart"),
+        // width: 580,
+        height: 250,
+        series: [ {
+            name: 'Value',
+            color: 'steelblue',
+            data: data
+        } ]
+    } );
+    var xAxis = new Rickshaw.Graph.Axis.Time({
+        graph: graph,
+        timeFixture: new Rickshaw.Fixtures.Time.Local()
+    });
+    var yAxis = new Rickshaw.Graph.Axis.Y({graph: graph});
+    var hoverDetail = new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        xFormatter: function(x) {
+            return new Date(x * 1000).toString();
+    }});
+    graph.render();
+
+    var slider = new Rickshaw.Graph.RangeSlider({
+        graph: graph,
+        element: document.querySelector('#slider')
+    });
+}
+
+function render_response(data, element) {
+    links_panel = $("<div/>").addClass("panel").addClass("panel-default");
+    element.append(links_panel);
+
+    links_title = $("<div/>").addClass("panel-heading").text("Links");
+    links_panel.append(links_title);
+    rels_ul = $("<ul/>").addClass("list-group");
+    links_panel.append(rels_ul);
+
+    $.each(data._links, function(rel, link) {
+        if(rel != "self" && rel != "curies") {
+            cell = $("<li/>").addClass("list-group-item");
+            rels_ul.append(cell);
+            cell.append(rel + ": ");
+            if($.isArray(link)) {
+                ul = $("<ul/>").addClass("list-group");
+                cell.append(ul)
+                $.each(link, function(i, link_item) {
+                    li = $("<li/>").addClass("list-group-item");
+                    ul.append(li)
+                    dest_link = $("<a/>").text(link_item.title).attr("href", link_item.href);
+                    li.append(dest_link);
+                })
+            }
+            else {
+                // link is just a link, not a list of links
+                dest_link = $("<a>").text(link.title).attr("href", link.href);
+                cell.append(dest_link);
+            }
+        }
+    })
+
+    attrs_panel = $("<div/>").addClass("panel").addClass("panel-default");
+    element.append(attrs_panel);
+    attrs_title = $("<div/>").addClass("panel-heading").text("Attributes");
+    attrs_panel.append(attrs_title);
+    attrs_ul = $("<ul/>").addClass("list-group");
+    attrs_panel.append(attrs_ul);
+    $.each(data, function(key, val) {
+        if(key != "_links" && key != "_embedded") {
+            cell = $("<li/>").addClass("list-group-item");
+            attrs_ul.append(cell);
+            cell.append(key + ": ");
+            if(key != "data") {
+                cell.append(val.toString());
+            }
+        }
+    })
+}
