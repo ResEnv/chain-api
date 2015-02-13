294fbaf4329f0ffe6a3a8cc66356ecf45f5ea1f5
Spencer Russell <spencer.f.russell@gmail.com>
2015-02-13 20:46:45 +0400

    prettifies logging, disables debug mode
    also fixes issue where Flask would complain that our websocket handler
    wasn't returning a response. Now we just return an empty response.

--- chain/websocketd.py
+++ chain/websocketd.py
@@ -8,19 +8,25 @@ import zmq.green as zmq
 import gevent
 from flask import Flask
 from flask_sockets import Sockets
-from geventwebsocket import WebSocketError
 from zmq_passthrough import passthrough
 from chain.settings import ZMQ_PASSTHROUGH_URL_PUB
+import logging
+import coloredlogs
 
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 zmq_ctx = zmq.Context()
 websockets = Sockets(app)
+
+coloredlogs.install(logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info("websocketd.py started")
 
 
 @websockets.route('/<tag>')
 def site_socket(ws, tag):
-    print('ws client connected for tag "%s"' % tag)
+    logger.info('ws client connected for tag "%s"' % tag)
     zmq_sock = zmq_ctx.socket(zmq.SUB)
     zmq_sock.connect(ZMQ_PASSTHROUGH_URL_PUB)
     # note that flask gives us tag as a unicode string
@@ -30,13 +36,16 @@ def site_socket(ws, tag):
         # in case we later subscribe to multiple topic, get the topic from the
         # incoming message
         msg_tag, _, msg = in_data.partition(' ')
-        print('Received on tag "%s": %s' % (msg_tag, msg))
+        logger.debug('Received on tag "%s": %s' % (msg_tag, msg))
         try:
             ws.send(msg)
-        except WebSocketError as e:
-            print('Caught WebSocketError: %s' % e)
+        except Exception as e:
+            logger.info('Caught Error sending to client: %s' % e)
             break
+    logger.info('Disconnecting ZMQ Socket for tag "%s"' % tag)
     zmq_sock.disconnect(ZMQ_PASSTHROUGH_URL_PUB)
+    # return an empty response so Flask doesn't complain
+    return ''
 
 # Start the passthrough process:
 gevent.spawn(passthrough, zmq_context=zmq_ctx)
