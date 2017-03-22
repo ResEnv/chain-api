809224ff6f1a65f9bd265393682453d3e8b231c7
Ella Chao <hchao@wellesley.edu>
2017-03-22 13:21:20 -0400

    fix migration script

--- postgres_to_influx.py
+++ postgres_to_influx.py
@@ -7,13 +7,41 @@ import sys
 
 # needs to be run from the manage.py shell context
 
-def get_points(offset, limit):
+BATCH_SIZE = 5000
+FIRST_TIMESTAMP = datetime.utcfromtimestamp(
+    float(1481811788574987776)/1e9).replace(tzinfo=timezone.utc)
+
+def get_points(offset, limit=None):
     '''Returns objects between offset and offset+limit'''
-    print('Start moving objects[{0}:{1}]'.format(
+    if limit == None:
+        data = ScalarData.objects.filter(
+        timestamp__lt=FIRST_TIMESTAMP.isoformat()).order_by('timestamp')
+        print('Start moving objects[{0}:]'.format(offset))
+    else:
+        print('Start moving objects[{0}:{1}]'.format(
+            offset,
+            offset+limit))
+        data = ScalarData.objects.order_by('timestamp')[offset:offset+limit]
+
+    if len(data)<BATCH_SIZE:
+        status_code = post_points_wrapper(data)
+    else:
+        count = 0
+        while count < len(data):
+            if len(data) - count >= BATCH_SIZE:
+                post_points(data[count:count+BATCH_SIZE])
+                count += BATCH_SIZEx
+            else:
+                post_points(data[count:])
+          
+        
+    return 
+
+def post_points_wrapper(data, offset, limit):
+    print('Moving objects[{0}:{1}]'.format(
         offset,
         offset+limit))
-    data = ScalarData.objects.all()[offset:offset+limit]
-    status_code = post_points(data)
+    status_code = post_points(data[offset:offset+limit])
     if status_code != HTTP_STATUS_SUCCESSFUL_WRITE:
         print('Failed to move objects[{0}:{1}]'.format(
             offset,
@@ -22,14 +50,12 @@ def get_points(offset, limit):
         print('Moved objects[{0}:{1}]'.format(
             offset,
             offset+limit))
-    return 
-
+    
 def post_points(list_of_points):
     '''Posts a list of data points to postgres and returns status
     code of request'''
     data = ''
     for point in list_of_points:
-        print point['timestamp']
         timestamp = InfluxClient.convert_timestamp(point.timestamp)
         data += '{0},sensor_id={1} value={2} {3}\n'.format(
             influx_client._measurement,
@@ -44,8 +70,10 @@ def post_points(list_of_points):
 
 
 if __name__ == "__main__":
-    if len(sys.argv)==4:
-        get_points(sys.argv[1],sys.argv[2],sys.argv[3])
+    if len(sys.argv)==3:
+        get_points(sys.argv[1],sys.argv[2])
+    elif len(sys.argv)==2:
+        get_points(sys.argv[1])
     else:
-         print('usage: %s <start_time> <timedelta> <number per batch>' % sys.argv[0])
+         print('usage: %s <offset> [limit]' % sys.argv[0])
          sys.exit(1)
