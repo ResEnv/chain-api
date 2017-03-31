7fb54eeafbf791333360f3c680f00c20fe0c21c3
Spencer Russell <spencer.f.russell@gmail.com>
2017-03-31 21:17:51 +0000

    BACKUP BEFORE DEPLOYING - removes ScalarData model and migrates away table

---
+++ chain/core/migrations/0013_auto__del_scalardata__del_index_scalardata_sensor_timestamp.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'ScalarData'
+        db.delete_table(u'core_scalardata')
+
+        # Removing index on 'ScalarData', fields ['sensor', 'timestamp']
+        db.delete_index(u'core_scalardata', ['sensor_id', 'timestamp'])
+
+
+    def backwards(self, orm):
+        # Adding index on 'ScalarData', fields ['sensor', 'timestamp']
+        db.create_index(u'core_scalardata', ['sensor_id', 'timestamp'])
+
+        # Adding model 'ScalarData'
+        db.create_table(u'core_scalardata', (
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True, db_index=True)),
+            ('sensor', self.gf('django.db.models.fields.related.ForeignKey')(related_name='scalar_data', to=orm['core.ScalarSensor'])),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('value', self.gf('django.db.models.fields.FloatField')()),
+        ))
+        db.send_create_signal(u'core', ['ScalarData'])
+
+
+    models = {
+        u'core.device': {
+            'Meta': {'ordering': "['name']", 'unique_together': "(['site', 'name', 'building', 'floor', 'room'],)", 'object_name': 'Device'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'building': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'floor': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'geo_location': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.GeoLocation']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'room': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'devices'", 'to': u"orm['core.Site']"})
+        },
+        u'core.geolocation': {
+            'Meta': {'object_name': 'GeoLocation'},
+            'elevation': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'longitude': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'core.metric': {
+            'Meta': {'object_name': 'Metric'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        u'core.person': {
+            'Meta': {'object_name': 'Person'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'geo_location': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.GeoLocation']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'picture_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'rfid': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'people'", 'to': u"orm['core.Site']"}),
+            'twitter_handle': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'core.presencedata': {
+            'Meta': {'object_name': 'PresenceData'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'presense_data'", 'to': u"orm['core.Person']"}),
+            'present': ('django.db.models.fields.BooleanField', [], {}),
+            'sensor': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'presence_data'", 'to': u"orm['core.PresenceSensor']"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'core.presencesensor': {
+            'Meta': {'unique_together': "(['device', 'metric'],)", 'object_name': 'PresenceSensor'},
+            'device': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'presence_sensors'", 'to': u"orm['core.Device']"}),
+            'geo_location': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.GeoLocation']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'metadata': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'metric': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'presence_sensors'", 'to': u"orm['core.Metric']"})
+        },
+        u'core.scalarsensor': {
+            'Meta': {'unique_together': "(['device', 'metric'],)", 'object_name': 'ScalarSensor'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'device': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sensors'", 'to': u"orm['core.Device']"}),
+            'geo_location': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.GeoLocation']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'metadata': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'metric': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sensors'", 'to': u"orm['core.Metric']"}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sensors'", 'to': u"orm['core.Unit']"})
+        },
+        u'core.site': {
+            'Meta': {'object_name': 'Site'},
+            'geo_location': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.GeoLocation']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'raw_zmq_stream': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        u'core.statusupdate': {
+            'Meta': {'object_name': 'StatusUpdate'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'status_updates'", 'to': u"orm['core.Person']"}),
+            'status': ('django.db.models.fields.TextField', [], {}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'core.unit': {
+            'Meta': {'object_name': 'Unit'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['core']
--- chain/core/models.py
+++ chain/core/models.py
@@ -97,27 +97,6 @@ class ScalarSensor(models.Model):
         return self.metric.name
 
 
-class ScalarData(models.Model):
-    '''A data point representing scalar sensor data, such as temperature,
-    humidity, etc.'''
-    # Django automatically creates indices on foreign keys
-    sensor = models.ForeignKey(ScalarSensor, related_name='scalar_data')
-    timestamp = models.DateTimeField(default=timezone.now, blank=True,
-                                     db_index=True)
-    value = models.FloatField()
-
-    class Meta:
-        verbose_name_plural = "scalar data"
-        index_together = [['sensor', 'timestamp']]
-
-    def __repr__(self):
-        return 'ScalarData(timestamp=%r, value=%r, sensor=%r)' % (
-            self.timestamp, self.value, self.sensor)
-
-    def __str__(self):
-        return '%.3f %s' % (self.value, self.sensor.unit)
-
-
 class Person(models.Model):
     '''A Person involved with the site. Some sensors might detect presence of a
     person, so they can reference this model with person-specific
