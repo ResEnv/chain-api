f87a3077b99261f545956b1eb1cf3dc9dde6d429
Spencer Russell <spencer.f.russell@gmail.com>
2017-12-18 17:51:44 -0500

    now dev docker targets remove themselves on exit

--- docker/dev/debug_dev.sh
+++ docker/dev/debug_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i -t -p 8000:8000 -p 9001:9001 -p 8080:80 -v "$(pwd)/../../:/opt/chain-api-dev" chain/dev bash
+docker run -it --rm -p 8000:8000 -p 9001:9001 -p 8080:80 -v "$(pwd)/../../:/opt/chain-api-dev" chain/dev bash
